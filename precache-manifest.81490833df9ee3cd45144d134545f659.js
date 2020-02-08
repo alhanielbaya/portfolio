9b4adb6d50ee47858318fa4d276381ee739dce87
alhanielbaya <alstrom201@gmail.com>
2020-02-07 18:51:41 -0800

    Updates

---
+++ precache-manifest.81490833df9ee3cd45144d134545f659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db9f8a7b2560d14a30edbcc3b6be22d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8d925fee686d40f2035f",
+    "url": "/portfolio/static/css/2.059f48f8.chunk.css"
+  },
+  {
+    "revision": "807d775d210882552daa",
+    "url": "/portfolio/static/css/main.d8a03ff9.chunk.css"
+  },
+  {
+    "revision": "8d925fee686d40f2035f",
+    "url": "/portfolio/static/js/2.f66b8383.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.f66b8383.chunk.js.LICENSE"
+  },
+  {
+    "revision": "807d775d210882552daa",
+    "url": "/portfolio/static/js/main.80c3d8bc.chunk.js"
+  },
+  {
+    "revision": "8ab5d261c3a40cd48557",
+    "url": "/portfolio/static/js/runtime-main.e01f936b.js"
+  }
+]);
